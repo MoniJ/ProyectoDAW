87f2fa09de362c4047bed5de04306a2c5f881914
mpradoa94 <mpradoa94@gmail.com>
2015-03-19 17:29:35 -0600

    Update generic.php

--- generic.php
+++ generic.php
@@ -115,8 +115,8 @@
 				<nav id="nav">
 					<ul>
 						<li><a href="index.html">Home</a></li>
-						<li><a href="generic.html">Generic</a></li>
-						<li><a href="elements.html">Elements</a></li>
+						<li><a href="generic.php">Comida</a></li>
+						<li><a href="elements.html">IMC</a></li>
 						<li><a href="#" class="button special">Sign Up</a></li>
 					</ul>
 				</nav>
